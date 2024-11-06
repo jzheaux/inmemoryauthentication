4bd33ae22eb4a0affa724b66ccc4357b6441bfdf
Josh Cummings <josh.cummings@gmail.com>
2024-11-06 12:38:34 -0700

    Polish Spring Security Usage

--- src/main/java/com/example/filterchain3/configuration/SecurityConfig.java
+++ src/main/java/com/example/filterchain3/configuration/SecurityConfig.java
@@ -1,53 +1,58 @@
 package com.example.filterchain3.configuration;
 
-import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.stereotype.Component;
 
-import java.net.http.HttpRequest;
 @Slf4j
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager()
+    public UserDetailsService users()
     {
         UserDetails user = User.withDefaultPasswordEncoder()
                 .username("user")
                 .password("user")
-                .roles("user")
+                .authorities("user")
                 .build();
-        UserDetails admin= User.withDefaultPasswordEncoder()
+        UserDetails admin = User.withDefaultPasswordEncoder()
                 .username("admin")
                 .password("admin")
-                .roles("admin")
+                .authorities("admin")
                 .build();
-          return  new InMemoryUserDetailsManager(user,admin);
+        return new InMemoryUserDetailsManager(user, admin);
     }
+
     @Bean
-   public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception
-   {   log.info("inside security filter chain");
-       http                                  .csrf(AbstractHttpConfigurer::disable);
-       http                                  .authorizeHttpRequests((req)->req
-                                             .requestMatchers("/admin").hasRole("admin")
-                                              .requestMatchers("/user").hasRole("user")
-                                              .requestMatchers("/hello").permitAll()
-                                             .anyRequest().authenticated());
-       http                                    .httpBasic(Customizer.withDefaults());
-       return http.build();
-   }
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+    {
+        log.info("constructing security filter chain");
+        http
+                .authorizeHttpRequests((req) -> req
+                    .requestMatchers("/admin").hasAuthority("admin")
+                    .requestMatchers("/user").hasAuthority("user")
+                    .requestMatchers("/hello").permitAll()
+                    .anyRequest().denyAll()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+        RoleHierarchyImpl roles = new RoleHierarchyImpl();
+        roles.setHierarchy("admin > user");
+        return roles;
+    }
 }
--- src/test/java/com/example/filterchain3/Filterchain3ApplicationTests.java
+++ src/test/java/com/example/filterchain3/Filterchain3ApplicationTests.java
@@ -1,13 +1,41 @@
 package com.example.filterchain3;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class Filterchain3ApplicationTests {
 
+	@Autowired
+	MockMvc mvc;
+
 	@Test
-	void contextLoads() {
+	@WithMockUser(authorities="user")
+	void whenUserHasUserRoleThenSuccess() throws Exception {
+		this.mvc.perform(get("/user"))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	@WithMockUser
+	void whenUserRoleIsMissingThenDenied() throws Exception {
+		this.mvc.perform(get("/user"))
+				.andExpect(status().isForbidden());
+	}
+
+	@Test
+	@WithMockUser(authorities="admin")
+	void whenUserHasAdminRoleThenSuccess() throws Exception {
+		this.mvc.perform(get("/user"))
+				.andExpect(status().isOk());
 	}
 
 }
